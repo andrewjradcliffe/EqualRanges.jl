84a0570dbc266aa8ed0b0946b823c1f8198d7b95
Andrew Radcliffe <andrew.radcliffe@intel.com>
2022-04-20 15:33:07 -0700

    Add equalranges tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,25 @@ using EqualRanges
 using Test
 
 @testset "EqualRanges.jl" begin
-    # Write your tests here.
+    @testset "equalranges" begin
+        M, N = 7, 22
+        q, r = divrem(N, M)
+        easy = [mthpair(q, r, M, m) for m = 1:M]
+        fast = equalpairs(q, r, M)
+        @test easy == fast
+        easyr = [mthrange(q, r, M, m) for m = 1:M]
+        fastr = equalranges(q, r, M)
+        @test easyr == fastr
+        @test sum(length, easyr) == N == sum(length, fastr)
+        #
+        M, N = 12, 10
+        rr = equalranges(N, M)
+        @test sum(length, rr) == N
+        #
+        start, stop = 5:16
+        ur = start:stop
+        rs = equalranges(ur, 4)
+        @test sum(length, rs) == length(ur)
+    end
+
 end

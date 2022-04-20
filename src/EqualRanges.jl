3adcf4da0c00ed2b4707c515b9109b612dc55c99
Andrew Radcliffe <andrew.radcliffe@intel.com>
2022-04-20 16:27:42 -0700

    Update exports

--- src/EqualRanges.jl
+++ src/EqualRanges.jl
@@ -1,7 +1,11 @@
 module EqualRanges
 
 export mthpair, mthrange, equalpairs, equalranges
+export splitranges
+export binaryranges, binaryranges!
 
 include("equalranges.jl")
+include("splitranges.jl")
+include("binaryranges.jl")
 
 end
